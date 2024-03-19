421a8bd6303645b748004ecdcb4ae7409c988681
Christian Dev <crislm1322@gmail.com>
2024-03-19 16:49:50 -0500

    Integration of Prisma ORM in Nestjs Framework

---
+++ src/articles/entities/article.entity.ts
@@ -0,0 +1,25 @@
+import { Article } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class ArticleEntity implements Article {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  title: string;
+
+  @ApiProperty()
+  description: string | null;
+
+  @ApiProperty()
+  body: string;
+
+  @ApiProperty()
+  published: boolean;
+
+  @ApiProperty()
+  createAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+}
